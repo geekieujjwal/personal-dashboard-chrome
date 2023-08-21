836a35bc74fdb3078850e6ab7269bbd1a11be916
Ujjwal Jaguri <ujjwal.jaguri.19012003@gmail.com>
2023-08-21 22:33:24 +0530

    fixed: responsiveness(CSS) by adding a div| formatted: html and js file by adding comments

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
+// Fetch a random landscape nature photo from Unsplash API
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
 )
   .then((res) => res.json())
   .then((data) => {
+    // Set the fetched photo as the background image
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
-    // document.getElementById("author").textContent = `By: ${data.user.name}`;
+    // Display the photo's author and a link to the Todo app
     document.getElementById("author").innerHTML = `
-                <p>By: ${data.user.name}</p>
-                <a href="https://todo-sol.vercel.app/" target="_blank">Todo</a>
-                `;
+      <p>By: ${data.user.name}</p>
+      <a href="https://todo-sol.vercel.app/" target="_blank">Todo</a>
+    `;
   })
   .catch((err) => {
-    // Use a default background image/author
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`;
-    // document.getElementById("author").textContent = `By: Dodi Achmad`;
+    // Use a default background image/author in case of an error
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`;
     document.getElementById("author").innerHTML = `
-                <p>By: Dodi Achmad</p>
-                <p><a href="https://todo-sol.vercel.app/"></a>Todo</p>
-                `;
+      <p>By: Dodi Achmad</p>
+      <p><a href="https://todo-sol.vercel.app/">Todo</a></p>
+    `;
   });
 
+// Fetch Bitcoin cryptocurrency data from CoinGecko API
 fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
   .then((res) => {
     if (!res.ok) {
@@ -29,18 +30,21 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
     return res.json();
   })
   .then((data) => {
+    // Display Bitcoin's logo and name
     document.getElementById("crypto-top").innerHTML = `
-            <img src=${data.image.small} />
-            <span>${data.name}</span>
-        `;
+      <img src=${data.image.small} />
+      <span>${data.name}</span>
+    `;
+    // Display cryptocurrency price details
     document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
-        `;
+      <p>🎯: $${data.market_data.current_price.usd}</p>
+      <p>👆: $${data.market_data.high_24h.usd}</p>
+      <p>👇: $${data.market_data.low_24h.usd}</p>
+    `;
   })
   .catch((err) => console.error(err));
 
+// Update the displayed time every second
 function getCurrentTime() {
   const date = new Date();
   document.getElementById("time").textContent = date.toLocaleTimeString(
@@ -48,9 +52,9 @@ function getCurrentTime() {
     { timeStyle: "medium" }
   );
 }
-
 setInterval(getCurrentTime, 1000);
 
+// Get current geolocation and fetch weather data
 navigator.geolocation.getCurrentPosition((position) => {
   fetch(
     `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
@@ -62,20 +66,22 @@ navigator.geolocation.getCurrentPosition((position) => {
       return res.json();
     })
     .then((data) => {
+      // Display weather icon, temperature, and city name
       const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       document.getElementById("weather").innerHTML = `
-                <img src=${iconUrl} />
-                <p class="weather-temp">${Math.round(data.main.temp)}ºC</p>
-                <p class="weather-city">${data.name}</p>
-            `;
+        <img src=${iconUrl} />
+        <p class="weather-temp">${Math.round(data.main.temp)}ºC</p>
+        <p class="weather-city">${data.name}</p>
+      `;
     })
     .catch((err) => console.error(err));
 });
 
+// Define API key and URL for getting quotes
 const apiKey = "W8jgTtYekrr5FXnzTGKlqw==qdOMT27Z5IKatjpX";
 const apiUrl = "https://api.api-ninjas.com/v1/quotes?category=attitude";
 
-// Fetch data using the fetch API
+// Fetch data using the fetch API with an API key
 fetch(apiUrl, {
   headers: {
     "X-Api-Key": apiKey,
